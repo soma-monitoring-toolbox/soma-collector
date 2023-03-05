e2d3e2dfa5a07de52f88e6a0119fabdd7b913f2b
Srinivasan Ramesh <ramesh.srinivasan09@gmail.com>
2023-03-04 19:28:10 -0800

    Delete build directory

--- build/src/CMakeFiles/soma-bedrock-module.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for soma-bedrock-module.
-# This may be replaced when dependencies are built.
--- build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /users/dewiy/soma-collector/src
-# Build directory: /users/dewiy/soma-collector/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
