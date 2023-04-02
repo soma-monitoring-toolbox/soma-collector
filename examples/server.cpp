faeaf1513f841ef8f5c94cc565a92078e924938b
Oskar Lappi <lappiosk@mahti-login12.mahti.csc.fi>
2023-04-02 19:17:00 +0300

    Changes to server to allow Astaroth integration

--- examples/server.cpp
+++ examples/server.cpp
@@ -23,7 +23,7 @@ static bool        g_use_progress_thread = false;
 
 static void parse_command_line(int argc, char** argv);
 
-static void setup_admin_nodes(tl::engine engine, std::string server_addr, int rank, int size) {
+static void setup_admin_nodes(tl::engine engine, std::string server_addr, int rank, int size, MPI_Comm comm) {
     ofstream addr_file;
 
     // Initialize the thallium server
@@ -42,7 +42,7 @@ static void setup_admin_nodes(tl::engine engine, std::string server_addr, int ra
 	    }
 	    addr_file.close();
 	}
-	MPI_Barrier(MPI_COMM_WORLD);
+	MPI_Barrier(comm);
     }
 
 }
@@ -52,59 +52,63 @@ int main(int argc, char** argv) {
     ofstream addr_file;
     parse_command_line(argc, argv);
     MPI_Barrier(MPI_COMM_WORLD);
-    int rank, size;
-    int key, color;
-    int new_rank;
-
+    
     tl::engine engine(g_address, THALLIUM_SERVER_MODE, g_use_progress_thread, g_num_threads);
     std::vector<snt::Provider> providers;
 
     engine.enable_remote_shutdown();
-    MPI_Comm new_comm;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    if(rank == 0) {
+
+    int instance_id;
+    try {
+        instance_id = std::stoi(std::string(getenv("SOMA_NUM_SERVER_INSTANCES")));
+    } catch (std::exception e){
+        instance_id = 0;
+    }
+
+    int soma_rank, soma_size;
+    int soma_instance_rank, soma_instance_size;
+
+    MPI_Comm soma_comm;
+    MPI_Comm_split(MPI_COMM_WORLD, 900 + instance_id, 0, &soma_comm);
+    MPI_Comm_rank(soma_comm, &soma_rank);
+    MPI_Comm_size(soma_comm, &soma_size);
+
+    MPI_Comm soma_instance_comm;
+    MPI_Comm_split(MPI_COMM_WORLD, 1000 + instance_id, 0, &soma_instance_comm);
+    MPI_Comm_rank(soma_instance_comm, &soma_instance_rank);
+    MPI_Comm_size(soma_instance_comm, &soma_instance_size);
+
+    if(soma_rank == 0) {
 	    addr_file.open(getenv("SOMA_SERVER_ADDR_FILE"), ios::app);
-	    addr_file << size << "\n";
+	    addr_file << soma_size << "\n";
 	    addr_file.close();
     }
 
    
     int num_instances = std::stoi(std::string(getenv("SOMA_NUM_SERVER_INSTANCES")));
 
-    if(num_instances == 1) {
-        for(int i = 0; i < size; i++) {
-	    if(rank == i) {
-		    addr_file.open(getenv("SOMA_SERVER_ADDR_FILE"), ios::app);
-		    addr_file << rank << " " << (std::string)engine.self() << "\n";
-		    addr_file.close();
-	    }
-	    MPI_Barrier(MPI_COMM_WORLD);
-        }
-        MPI_Comm_dup(MPI_COMM_WORLD, &new_comm);
-    } else {
-        key = rank;
-        color = (int)(rank/(size/num_instances));
-        MPI_Comm_split(MPI_COMM_WORLD, color, key, &new_comm);
-        MPI_Comm_rank(new_comm, &new_rank);
+    char * filename = getenv("SOMA_SERVER_ADDR_FILE");
 
-        for(int i = 0; i < size; i++) {
-	    if(rank == i) {
-		    addr_file.open(getenv("SOMA_SERVER_ADDR_FILE"), ios::app);
-		    addr_file << new_rank << " " << (std::string)engine.self() << "\n";
-		    addr_file.close();
-	    }
-	    MPI_Barrier(MPI_COMM_WORLD);
-        }
+    for(int i = 0; i < soma_size; i++) {
+	if(soma_rank == i) {
+	    addr_file.open(getenv("SOMA_SERVER_ADDR_FILE"), ios::app);
+	    addr_file << soma_rank << " " << (std::string)engine.self() << "\n";
+	    addr_file.close();
+	}
+	MPI_Barrier(soma_comm);
     }
-       
-    for(unsigned i=0 ; i < g_num_providers; i++) {
-        providers.emplace_back(engine, i, new_comm);
+      
+    for(size_t i = 0 ; i < g_num_providers; i++) {
+        providers.emplace_back(engine, i, soma_instance_comm);
     }
 
+    //Not sure why this sync is needed...
+    MPI_Barrier(soma_comm);
+    setup_admin_nodes(engine, (std::string)engine.self(), soma_rank, soma_size, soma_comm);
+
+    //Sync with client
     MPI_Barrier(MPI_COMM_WORLD);
-    setup_admin_nodes(engine, (std::string)engine.self(), rank, size);
 
     engine.wait_for_finalize();
     MPI_Finalize();
